9f463e3c0794aed0bccb7eb98227eb569a29af1f
Yuno <chaturvediaditya27@gmail.com>
2025-09-16 22:32:15 +0530

    Clean up tracked files based on .gitignore

--- server/utility/keys.js
+++
@@ -1,6 +0,0 @@
-const JWT_SECRET = "Mohit1234";
-const MONGO_URL = "mongodb://localhost:27017/Carbon"
-const MAIL_PASS= "ogck czec qixt kfuw"
-const MAIL_ID= "geopulse.noreply@gmail.com" //sender mail 
-
-module.exports = { JWT_SECRET, MONGO_URL,MAIL_PASS, MAIL_ID };
